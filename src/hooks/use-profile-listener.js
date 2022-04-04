1e27718b7e5a91190914c9601006546c9599e97b
dkvaja <dushyant@logisticinfotech.co.in>
2022-04-04 23:32:42 +0530

    fix issue

--- src/hooks/use-profile-listener.js
+++ src/hooks/use-profile-listener.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import { auth } from "../firebase/config";
 import { listenToMyProfile } from "../services/listeners";
